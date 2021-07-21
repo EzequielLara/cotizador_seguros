26c99d174aec50cf50ff38a23d47961f6f17f4f4
Ezequiel <ezekdaw@gmail.com>
2021-07-21 14:08:35 +0200

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import { Fragment } from "react";
 import './index.css';
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import styled from 'styled-components';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
 
 function App() {
   return (
    <Fragment>
+    <Contenedor>
       <Header
         titulo = 'Cotizador de Seguros'
       />
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
+    </Contenedor>
    </Fragment>
   );
 }
